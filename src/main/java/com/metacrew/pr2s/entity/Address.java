1c9b9d3349710fbf8ff752445d71265005487b29
nohsunggyu <gkdlshtjdrb@naver.com>
2022-06-12 19:14:39 +0900

    Address Entity 주석 추가

--- src/main/java/com/metacrew/pr2s/entity/Address.java
+++ src/main/java/com/metacrew/pr2s/entity/Address.java
@@ -9,6 +9,12 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
+/**
+ * Address 테이블과 매핑되는 엔티티이다.
+ * 주소 정보를 가지고 있다.
+ * @author sunggyu
+ * @since 2022.06.12
+ */
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
