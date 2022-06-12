d3e8f2e5a33a04ccb1d0a1a73c0ca1a921b1e5bf
nohsunggyu <gkdlshtjdrb@naver.com>
2022-06-12 19:15:11 +0900

    Content Entity 매핑 예시 작성

--- src/main/java/com/metacrew/pr2s/entity/Content.java
+++ src/main/java/com/metacrew/pr2s/entity/Content.java
@@ -4,11 +4,14 @@ import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
 
+/**
+ * Content 테이블과 매핑되는 엔티티이다.
+ * 게시글 정보를 수정 및 조회할 수 있다.
+ * @author sunggyu
+ * @since 2022.06.12
+ */
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
@@ -16,4 +19,8 @@ public class Content {
     @Id
     @Column(name = "content_id") @GeneratedValue
     private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "board_id")
+    private Board board;
 }
