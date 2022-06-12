10fcaf2105b7f676397c409a91e12442a0524697
nohsunggyu <gkdlshtjdrb@naver.com>
2022-06-12 18:08:23 +0900

    베이스 엔티티 생성

--- src/main/java/com/metacrew/pr2s/entity/base/BaseEntity.java
+++ src/main/java/com/metacrew/pr2s/entity/base/BaseEntity.java
@@ -2,10 +2,11 @@ package com.metacrew.pr2s.entity.base;
 
 import lombok.Getter;
 
+import javax.persistence.Column;
 import javax.persistence.MappedSuperclass;
 
 @MappedSuperclass
 @Getter
 public class BaseEntity extends BaseTimeEntity{
-
+    private boolean isDeleted;
 }
