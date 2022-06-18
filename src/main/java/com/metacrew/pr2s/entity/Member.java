acc82df12d6476d0aa06817ad8d1b1b224a26ad0
nohsunggyu <gkdlshtjdrb@naver.com>
2022-06-18 20:19:19 +0900

    DOCS : Member 엔티티 주석 작성

--- src/main/java/com/metacrew/pr2s/entity/Member.java
+++ src/main/java/com/metacrew/pr2s/entity/Member.java
@@ -9,6 +9,11 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
+/**
+ * Member 테이블과 매핑되는 엔티티이다.
+ * @author sunggyu
+ * @since 2022.06.17
+ */
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
