3512d0dd7efd7cea91924f2f975d4ad2bcab8b6f
nahyunoh <onheh28@gmail.com>
2022-06-23 21:46:17 +0900

    FEAT : Comment 테이블 엔티티 매핑

--- src/main/java/com/metacrew/pr2s/entity/Comment.java
+++ src/main/java/com/metacrew/pr2s/entity/Comment.java
@@ -1,19 +1,45 @@
 package com.metacrew.pr2s.entity;
 
+import com.metacrew.pr2s.entity.base.BaseEntity;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+/**
+ * Comment 테이블과 매핑되는 엔티티이다.
+ * @author nahyun
+ * @since 2022.06.23
+ */
 
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
-public class Comment {
+public class Comment extends BaseEntity {
     @Id
     @Column(name = "comment_id") @GeneratedValue
     private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "room_id")
+    private Room room;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "board_id")
+    private Board board;
+
+    @Column
+    private String content;
+
+    @Column
+    private Boolean isCommentedBoard;
+
+    // TODO: 2022-06-23 답글 자체조인 가능 여부 확인
 }
