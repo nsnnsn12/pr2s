9163598fc4185e7404237c9e487ff063be3a7a5e
hyeonwoopark <yey6687@daum.net>
2022-06-23 20:41:01 +0900

    FEAT : Room.java 수정

--- src/main/java/com/metacrew/pr2s/entity/Room.java
+++ src/main/java/com/metacrew/pr2s/entity/Room.java
@@ -1,24 +1,40 @@
 package com.metacrew.pr2s.entity;
 
+import com.metacrew.pr2s.entity.base.BaseEntity;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.time.LocalDateTime;
 
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
-public class Room {
+public class Room extends BaseEntity {
     @Id
     @Column(name = "room_id") @GeneratedValue
     private Long id;
 
+    @JoinColumn(name = "address_id")
+    @OneToOne
+    private Address address;
+
+    @JoinColumn(name = "institution_id")
+    @ManyToOne
+    private Institution institution;
+
+    @JoinColumn(name = "file_id")
+    @ManyToOne(fetch = FetchType.LAZY)
+    private File file;
+
     @Column
     private String title;
-    
-    //// TODO: 2022-06-16 File 매핑작업 필요 
+
+    @Column
+    private String description;
+
+    @Column
+    private int maximumPersonCount;
+
 }
