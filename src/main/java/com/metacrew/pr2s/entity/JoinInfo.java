dff7c5f53e03569c4770bb57d232f36a5c873f63
nahyunoh <onheh28@gmail.com>
2022-06-23 21:47:28 +0900

    FEAT : JoinInfo 테이블 엔티티 매핑

--- src/main/java/com/metacrew/pr2s/entity/JoinInfo.java
+++ src/main/java/com/metacrew/pr2s/entity/JoinInfo.java
@@ -1,19 +1,44 @@
 package com.metacrew.pr2s.entity;
 
+import com.metacrew.pr2s.entity.base.BaseEntity;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+/**
+ * JoinInfo 테이블과 매핑되는 엔티티이다.
+ * @author nahyun
+ * @since 2022.06.23
+ */
 
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
-public class JoinInfo {
+public class JoinInfo extends BaseEntity {
     @Id
     @Column(name = "join_info_id") @GeneratedValue
     private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "institution_id")
+    private Institution institution;
+
+    @Column
+    private Boolean isApproved;
+
+    @Column
+    private LocalDateTime joinDate;
+
+    @Column
+    private LocalDateTime resignDate;
+
+    @Column
+    private Boolean isForceResigned;
 }
