0afa910abb71cdd4bee730776fff0eca4008a22f
nohsunggyu <gkdlshtjdrb@naver.com>
2022-06-25 17:06:04 +0900

    FEAT : 게시물 관련 service 정의

---
+++ src/main/java/com/metacrew/pr2s/service/ArticleService.java
@@ -0,0 +1,14 @@
+package com.metacrew.pr2s.service;
+/**
+ * 게시물과 관련된 서비스 로직을 가지고 있는 클래스이다.
+ * @author sunggyu
+ * @since 2022.06.25
+ */
+public interface ArticleService {
+    // TODO: 2022-06-25 주석은 코드 구현하면서 리턴값과 입력값이 정해질 때 작성한다.
+
+    void insert();
+    void update();
+    void search();
+    void delete();
+}
