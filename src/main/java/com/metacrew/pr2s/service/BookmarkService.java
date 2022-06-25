eff22e95958e1039e867a1a1f8e7259bb0d9e938
nohsunggyu <gkdlshtjdrb@naver.com>
2022-06-25 18:28:13 +0900

    FEAT : bookmark 관련 service 정의

--- src/main/java/com/metacrew/pr2s/service/BookmarkService.java
+++ src/main/java/com/metacrew/pr2s/service/BookmarkService.java
@@ -6,10 +6,14 @@ package com.metacrew.pr2s.service;
  * @since 2022.06.25
  */
 public interface BookmarkService {
-    // TODO: 2022-06-25 주석은 코드 구현하면서 리턴값과 입력값이 정해질 때 작성한다.
-
-    void insert();
-    void update();
-    void search();
-    void delete();
+    // TODO: 2022-06-25 관심사항 등록 메소드
+    // TODO: 2022-06-25 관심사항 삭제 메소드
+    // TODO: 2022-06-25 관심사항 조회 메소드
+    // TODO: 2022-06-25 관심사항 수정 메소드
+    // TODO: 2022-06-25 생각해보아야 할 점
+    /*
+        기관 관심사항과, 방 관심사항에 대한 로직 처리를
+        동일한 메소드로 처리해야하는지
+        아니면 각각의 메소드로 처리해야 하는지지
+    */
 }
