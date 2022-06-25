c19e6300db4cf7e24ae285b2ab6a30f6a5ed745c
nohsunggyu <gkdlshtjdrb@naver.com>
2022-06-25 18:30:55 +0900

    FEAT : 예약을 관리하는데 있어 전반적으로 필요한 service 정의

---
+++ src/main/java/com/metacrew/pr2s/service/ReservationService.java
@@ -0,0 +1,9 @@
+package com.metacrew.pr2s.service;
+/**
+ * 예약의 대한 정보를 관리하기 위한 서비스 로직을 가지고 있는 클래스이다.
+ * @author sunggyu
+ * @since 2022.06.25
+ */
+public interface ReservationService {
+    // TODO: 2022-06-25 예약현황 조회 메소드
+}
