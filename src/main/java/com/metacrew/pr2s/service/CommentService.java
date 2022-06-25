fcb818d1a09388d49aa0dd38524430fb109a312c
nohsunggyu <gkdlshtjdrb@naver.com>
2022-06-25 18:28:32 +0900

    FEAT : 댓글 관련 service 정의

---
+++ src/main/java/com/metacrew/pr2s/service/CommentService.java
@@ -0,0 +1,12 @@
+package com.metacrew.pr2s.service;
+/**
+ * 댓글과 관련된 서비스 로직을 가지고 있는 클래스이다.
+ * @author sunggyu
+ * @since 2022.06.25
+ */
+public interface CommentService {
+    // TODO: 2022-06-25 댓글 등록 메소드
+    // TODO: 2022-06-25 댓글 삭제 메소드
+    // TODO: 2022-06-25 댓글 수정 메소드
+    // TODO: 2022-06-25 댓글 조회 메소드
+}
