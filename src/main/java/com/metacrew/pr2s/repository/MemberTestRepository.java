6be128d94f04b71a17d8c71c9e3080e80eade13a
nohsunggyu <gkdlshtjdrb@naver.com>
2022-06-29 22:10:53 +0900

    FEAT : JPQL 학습을 위한 샘플용 Repository Method 추가

--- src/main/java/com/metacrew/pr2s/repository/MemberTestRepository.java
+++ src/main/java/com/metacrew/pr2s/repository/MemberTestRepository.java
@@ -1,13 +1,28 @@
 package com.metacrew.pr2s.repository;
 
+import com.metacrew.pr2s.entity.Member;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
+import java.util.List;
 
+/**
+ * JPQL 학습을 위한 테스트 Repository 클래스이다.
+ * @author sunggyu
+ * @since 2022.06.29
+ */
 @Repository
 @RequiredArgsConstructor
 public class MemberTestRepository {
     private final EntityManager em;
+    public void save(Member member){
+        em.persist(member);
+    }
 
+    public List<Member> findByLoginId(String loginId){
+        return em.createQuery("select m from Member m where m.loginId = :loginId", Member.class)
+                .setParameter("loginId", loginId)
+                .getResultList();
+    }
 }
