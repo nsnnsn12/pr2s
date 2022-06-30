b13e210013a31bb6399890dae6d878bd66b0969a
nohsunggyu <gkdlshtjdrb@naver.com>
2022-06-30 20:14:15 +0900

    FEAT : 게시판 관리 서비스 기능 추가

---
+++ src/main/java/com/metacrew/pr2s/service/BoardService.java
@@ -0,0 +1,13 @@
+package com.metacrew.pr2s.service;
+
+/**
+ * 관리자가 게시판을 관리하기 위한 서비스 로직을 가지고 있는 클래스이다.
+ * @author sunggyu
+ * @since 2022.06.30
+ */
+public interface BoardService {
+    // TODO: 2022-06-30 게시판 등록
+    // TODO: 2022-06-30 게시판 조회
+    // TODO: 2022-06-30 게시판 수정
+    // TODO: 2022-06-30 게시판 삭제
+}
