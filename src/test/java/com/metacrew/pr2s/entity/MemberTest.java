f1a30b3e8159001f63e883dec34930ba254512a8
nsnnsn12 <gkdlshtjdrb@naver.com>
2022-07-05 20:07:25 +0900

    REFACTOR : MemberTest 리팩토링

--- src/test/java/com/metacrew/pr2s/entity/MemberTest.java
+++ src/test/java/com/metacrew/pr2s/entity/MemberTest.java
@@ -1,27 +1,31 @@
 package com.metacrew.pr2s.entity;
 
 import com.metacrew.pr2s.dto.JoinMemberDto;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.*;
 
 class MemberTest {
     @Test
     public void overLoadingTest(){
+        //given
         JoinMemberDto joinMemberDto = new JoinMemberDto();
         joinMemberDto.setName("노성규");
         joinMemberDto.setPassword("1234");
         joinMemberDto.setLoginId("shtjdrb");
         joinMemberDto.setEmail("gkdlshtjdrb@naver.com");
         joinMemberDto.setTelNo("01012341234");
+
+        //when
         Member member = Member.createJoinMember(joinMemberDto);
-        Assertions.assertThat(member.getName()).isEqualTo("노성규");
-        Assertions.assertThat(member.getPassword()).isEqualTo("1234");
-        Assertions.assertThat(member.getLoginId()).isEqualTo("shtjdrb");
-        Assertions.assertThat(member.getEmail()).isEqualTo("gkdlshtjdrb@naver.com");
-        Assertions.assertThat(member.getTelNo()).isEqualTo("01012341234");
-        Assertions.assertThat(member.getAddress()).isNull();
-        Assertions.assertThat(member.getImageFile()).isNull();
+
+        //then
+        assertThat(member.getName()).isEqualTo("노성규");
+        assertThat(member.getPassword()).isEqualTo("1234");
+        assertThat(member.getLoginId()).isEqualTo("shtjdrb");
+        assertThat(member.getEmail()).isEqualTo("gkdlshtjdrb@naver.com");
+        assertThat(member.getTelNo()).isEqualTo("01012341234");
+        assertThat(member.getAddress()).isNull();
+        assertThat(member.getImageFile()).isNull();
     }
 }
