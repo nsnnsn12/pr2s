f77d0f6f85f4cd4de5e1ff29febffe9bc0072644
nsnnsn12 <gkdlshtjdrb@naver.com>
2022-07-07 22:41:09 +0900

    FEAT : MemberService 메소드 정의

--- src/main/java/com/metacrew/pr2s/service/memberservice/MemberService.java
+++ src/main/java/com/metacrew/pr2s/service/memberservice/MemberService.java
@@ -1,14 +1,79 @@
 package com.metacrew.pr2s.service.memberservice;
+
+import com.metacrew.pr2s.dto.AddressDto;
+import com.metacrew.pr2s.dto.JoinMemberDto;
+import com.metacrew.pr2s.dto.MyPageDto;
+import com.metacrew.pr2s.entity.Member;
+
 /**
- * 사용자가 자신의 정보를 관리하기 위한 서비스 로직을 가지고 있는 클래스이다.
+ * 회원 정보를 관리하기 위한 메소드를 정의한 인터페이스이다.
  * @author sunggyu
  * @since 2022.06.25
  */
 public interface MemberService {
-    // TODO: 2022-06-25 회원가입 메소드
-    // TODO: 2022-06-25 회원정보수정 메소드
-    // TODO: 2022-06-25 회원탈퇴 메소드
-    // TODO: 2022-06-25 기관 가입 요청 조회 메소드
-    // TODO: 2022-07-01 회원정보 조회
+    /**
+     * 회원가입
+     * 회원가입용 DTO와 Address Dto를 입력받아 회원가입 처리하고 회원 정보를 리턴한다.
+     *
+     * @param joinMember 회원 정보.
+     * @param addressDto 주소 정보
+     * @return 회원정보
+     * @author sunggyu
+     * @since 2022.07.07
+     */
+    Member join(JoinMemberDto joinMember, AddressDto addressDto);
+
+    /**
+     * 마이페이지 정보 조회
+     * 입력받은 id로 MyPage 정보를 조회하여 리턴한다.
+     * @author sunggyu
+     * @since 2022.07.07
+     * @param id Member table id
+     * @return MyPageDto 마이페이지 정보
+     */
+    MyPageDto getMyPageInfo(Long id);
+
+    /**
+     * 마이페이지 수정
+     * 마이페이지용 DTO와 Member table id 입력받아 정보 수정 후 Member table id를 리턴한다.
+     * @author sunggyu
+     * @since 2022.07.07
+     * @param myPageDto 마이페이지 정보.
+     * @param id Member table id
+     * @return Member table id
+     */
+    Long updateForMyPage(MyPageDto myPageDto, Long id);
+
+    /**
+     * 회원탈퇴
+     * Member table id 입력받아 회원탈퇴처리한다.
+     * @author sunggyu
+     * @since 2022.07.07
+     * @param id Member table id
+     */
+    void removeAccount(Long id);
+
+    /**
+     * 기관가입요청
+     * Member table id와  institution table id를 입력받아
+     * 기관 가입 처리 후 JoinInfo Table id값을 리턴한다.
+     * @author sunggyu
+     * @since 2022.07.07
+     * @param memberId Member table id
+     * @param institutionId institution table id
+     * @return JoinInfo Table id
+     */
+    Long requestJoinOfInstitution(Long memberId, Long institutionId);
+
+    /**
+     * 기관가입요청 취소
+     * JoinInfo Table id 입력받아 기관 가입요청을 취소한다.
+     * @author sunggyu
+     * @since 2022.07.07
+     * @param id JoinInfo table id
+     */
+    void cancelRequestedJoinOfInstitution(Long id);
+
+
 
 }
