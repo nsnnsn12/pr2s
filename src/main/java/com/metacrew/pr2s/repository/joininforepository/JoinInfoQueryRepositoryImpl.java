d8bbb57a4982c6d3ec4c6afd5c9d062b36d07813
nsnnsn12 <gkdlshtjdrb@naver.com>
2022-07-23 15:20:35 +0900

    FEAT : InstitutionMemberService 기관 내 가입 요청 조회 구현 중

--- src/main/java/com/metacrew/pr2s/repository/joininforepository/JoinInfoQueryRepositoryImpl.java
+++ src/main/java/com/metacrew/pr2s/repository/joininforepository/JoinInfoQueryRepositoryImpl.java
@@ -1,6 +1,7 @@
 package com.metacrew.pr2s.repository.joininforepository;
 
 import com.metacrew.pr2s.dto.condition.JoinInfoConditionDto;
+import com.metacrew.pr2s.entity.Institution;
 import com.metacrew.pr2s.entity.JoinInfo;
 import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQueryFactory;
@@ -16,16 +17,22 @@ public class JoinInfoQueryRepositoryImpl implements JoinInfoQueryRepository {
         this.queryFactory = new JPAQueryFactory(em);
     }
     @Override
-    public List<JoinInfo> search(JoinInfoConditionDto joinInfoConditionDto) {
+    public List<JoinInfo> search(Institution institution, JoinInfoConditionDto joinInfoConditionDto) {
+        //엔티티 값을 어떻게 받을 것인가?
         // TODO: 2022-07-23 지금은 승인여부의 대한 조건밖에 없지만 필요 조건 추가
         return queryFactory
                 .select(joinInfo)
                 .from(joinInfo)
-                .where(approvedEq(joinInfoConditionDto.getIsApproved()))
+                .where(institutionEq(institution)
+                        ,approvedEq(joinInfoConditionDto.getIsApproved()))
                 .fetch();
     }
 
     private BooleanExpression approvedEq(Boolean isApproved){
         return isApproved != null ? joinInfo.isApproved.eq(isApproved) : null;
+    }
+
+    private BooleanExpression institutionEq(Institution institution){
+        return institution != null ? joinInfo.institution.eq(institution) : null;
     }
 }
