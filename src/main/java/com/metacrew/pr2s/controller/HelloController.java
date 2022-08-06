122b35f7046c88b70fb7e891e435d4b298b6d7af
nsnnsn12 <gkdlshtjdrb@naver.com>
2022-08-06 21:19:06 +0900

    FEAT : StorageService 구현

--- src/main/java/com/metacrew/pr2s/controller/HelloController.java
+++ src/main/java/com/metacrew/pr2s/controller/HelloController.java
@@ -1,15 +1,24 @@
 package com.metacrew.pr2s.controller;
 
+import com.metacrew.pr2s.service.storage.StorageFileNotFoundException;
+import com.metacrew.pr2s.service.storage.StorageService;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.List;
 
 @Controller
 @Slf4j
+@RequiredArgsConstructor
 public class HelloController {
+    private final StorageService storageService;
+
     @GetMapping("/hello")
     public String hello(){
         return "common/body/main";
@@ -21,5 +30,21 @@ public class HelloController {
             log.info(id);
         }
         return "common/body/main";
+    }
+
+    @PostMapping("/")
+    public String handleFileUpload(@RequestParam("file") MultipartFile file,
+                                   RedirectAttributes redirectAttributes) {
+
+        storageService.store(file);
+        redirectAttributes.addFlashAttribute("message",
+                "You successfully uploaded " + file.getOriginalFilename() + "!");
+
+        return "redirect:/";
+    }
+
+    @ExceptionHandler(StorageFileNotFoundException.class)
+    public ResponseEntity<?> handleStorageFileNotFound(StorageFileNotFoundException exc) {
+        return ResponseEntity.notFound().build();
     }
 }
