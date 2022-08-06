ba962f9ceafe8a66bd33daf01a878f111085f6d8
nsnnsn12 <gkdlshtjdrb@naver.com>
2022-08-06 20:01:46 +0900

    FEAT : File upload 인터셉터 추가

---
+++ src/main/java/com/metacrew/pr2s/config/WebConfig.java
@@ -0,0 +1,20 @@
+package com.metacrew.pr2s.config;
+
+import com.metacrew.pr2s.interceptor.FileInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.multipart.commons.CommonsMultipartResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new FileInterceptor())
+                .addPathPatterns("/*");
+    }
+}
--- src/main/java/com/metacrew/pr2s/controller/HelloController.java
+++ src/main/java/com/metacrew/pr2s/controller/HelloController.java
@@ -1,13 +1,25 @@
 package com.metacrew.pr2s.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.List;
 
 @Controller
+@Slf4j
 public class HelloController {
     @GetMapping("/hello")
     public String hello(){
+        return "common/body/main";
+    }
+
+    @PostMapping("/testfile")
+    public String test(@RequestAttribute List<String> fileIds){
+        for(String id : fileIds){
+            log.info(id);
+        }
         return "common/body/main";
     }
 }
