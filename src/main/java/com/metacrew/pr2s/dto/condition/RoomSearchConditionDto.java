7e8d4d1e647ee27adb636cb2ee0d25ebbac5e3ed
nsnnsn12 <gkdlshtjdrb@naver.com>
2022-10-07 07:19:54 +0900

    FEAT : 방 조회 조건 dto 생성

---
+++ src/main/java/com/metacrew/pr2s/dto/condition/RoomSearchConditionDto.java
@@ -0,0 +1,10 @@
+package com.metacrew.pr2s.dto.condition;
+
+/**
+ * 방 조회시 사용되는 검색 조건 DTO.
+ * @author sunggyu
+ * @since 2022.10.07
+ */
+public class RoomSearchConditionDto {
+    // TODO: 2022-10-07 사용자 등급 별로 조회가 달라지는데 그의 따라 검색 조건도 나누어야 하는지 고민이 필요
+}
