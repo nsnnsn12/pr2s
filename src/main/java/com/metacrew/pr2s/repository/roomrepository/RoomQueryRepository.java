6f04b0971de402568293337bf7aa7fab16c9671c
nsnnsn12 <gkdlshtjdrb@naver.com>
2022-10-12 22:34:51 +0900

    REFACTOR : 방 조회 성능 향상을 위한 리팩토링

--- src/main/java/com/metacrew/pr2s/repository/roomrepository/RoomQueryRepository.java
+++ src/main/java/com/metacrew/pr2s/repository/roomrepository/RoomQueryRepository.java
@@ -5,9 +5,10 @@ import com.metacrew.pr2s.dto.condition.RoomSearchConditionDto;
 import com.metacrew.pr2s.dto.room.SearchedRoomDto;
 import com.metacrew.pr2s.entity.Institution;
 import com.metacrew.pr2s.entity.JoinInfo;
+import com.metacrew.pr2s.entity.Room;
 
 import java.util.List;
 
 public interface RoomQueryRepository {
-    List<SearchedRoomDto> search(RoomSearchConditionDto roomSearchConditionDto);
+    List<Room> search(RoomSearchConditionDto roomSearchConditionDto);
 }
--- src/main/java/com/metacrew/pr2s/repository/roomrepository/RoomQueryRepositoryImpl.java
+++ src/main/java/com/metacrew/pr2s/repository/roomrepository/RoomQueryRepositoryImpl.java
@@ -21,6 +21,8 @@ import static com.metacrew.pr2s.entity.QAddress.*;
 import static com.metacrew.pr2s.entity.QInstitutionAddress.institutionAddress;
 import static com.metacrew.pr2s.entity.QReservation.reservation;
 import static com.metacrew.pr2s.entity.QRoom.room;
+import static com.metacrew.pr2s.entity.QRoomImage.roomImage;
+import static com.metacrew.pr2s.entity.QRoomTag.roomTag;
 import static com.metacrew.pr2s.entity.QRoomUsage.roomUsage;
 
 
@@ -30,14 +32,14 @@ public class RoomQueryRepositoryImpl implements RoomQueryRepository {
         this.queryFactory = new JPAQueryFactory(em);
     }
     @Override
-    public List<SearchedRoomDto> search(RoomSearchConditionDto roomSearchConditionDto) {
+    public List<Room> search(RoomSearchConditionDto roomSearchConditionDto) {
         //리턴값을 dto로 받을 것인가? 일단 dto로 받고 해당 기능이 더 확장되면 다시 entity로 받는 것으로 해야 할까?
+        // 왜? 일대다 조인이 많아질 수록 row 갯수는 곱해짐. room의 usage가 10개이고 tag가 10개라고 하면 벌써 row가 100이 되어버림.
         return queryFactory
-                .select(Projections.constructor(SearchedRoomDto.class))
+                .select(room)
                 .from(room)
                 .join(room.institutionAddress, institutionAddress) //다대일
                 .join(institutionAddress.address, address) //일대일
-                .leftJoin(room, roomUsage.room) //일대다
                 .where(room.isDeleted.eq(false)
                         , likeTitle(roomSearchConditionDto.getTitle())
                         , goeMaximumPersonCount(roomSearchConditionDto.getMaximumPersonCount())
@@ -45,43 +47,11 @@ public class RoomQueryRepositoryImpl implements RoomQueryRepository {
                         , eqSggNm(roomSearchConditionDto.getSggNm())
                         , eqEmdNm(roomSearchConditionDto.getEmdNm())
                         , notInDatePeriod(roomSearchConditionDto.getStartDate(), roomSearchConditionDto.getEndDate())
-                        , inUsage(roomSearchConditionDto.getUsages()))
+                        , inUsage(roomSearchConditionDto.getUsages())
+                        , likeTag(roomSearchConditionDto.getTitle()))
                 .fetch();
     }
-/*
-join시 사용돼야 하는 entity
-room, address, reservation, usage 조인을 걸고 페이징 작업까지 해야 함.
 
-    room 조건
-    //제목
-    private String title; -> 제목으로 태그도 검색해야 함.
-
-    //사용인원
-    private int maximumPersonCount;
-
-    address 조건
-    //x좌표
-    private String entX;
-
-    //y좌표
-    private String entY;
-
-    //시도
-    private String siNm;
-
-    //시군구
-    private String sggNm;
-
-    //읍면동
-    private String emdNm;
-
-    reservation 조건
-    //예약 시작 시간
-    private LocalDateTime startDate;
-
-    //예약 종료 시간
-    private LocalDateTime endDate;
-*/
     //제목 조건
     private BooleanExpression likeTitle(String title){
         return StringUtils.hasText(title) ? room.title.like(title) : null;
@@ -124,12 +94,33 @@ room, address, reservation, usage 조인을 걸고 페이징 작업까지 해야
     }
 
     //방 사용 용도
+    //성능 향상을 위해 조건에 맞는 사용용도만 조회 후 방 id로 group by 하여 row 수를 줄인다.
     private BooleanExpression inUsage(List<Usage> usages){
-        return usages != null ? roomUsage.usage.in(usages): null;
+        if(usages != null){
+            return room.id.in(
+                    JPAExpressions
+                    .select(roomUsage.room.id)
+                    .from(roomUsage)
+                    .where(roomUsage.usage.in(usages))
+                    .groupBy(roomUsage.room.id)
+            );
+        }
+        return null;
     }
 
-    //select id from reservation where startDate > sss and group by room id
-    // TODO: 2022-10-11 태그 조건 필요
-
+    //타이틀 명을 이용하여 태그 조건 조회
+    //성능 향상을 위해 조건에 맞는 태그만 조회 후 방 id로 group by 하여 row 수를 줄인다.
+    private BooleanExpression likeTag(String title){
+        if(StringUtils.hasText(title)){
+            return room.id.in(
+                    JPAExpressions
+                    .select(roomTag.room.id)
+                    .from(roomTag)
+                    .where(roomTag.name.like(title))
+                    .groupBy(roomTag.room.id)
+            );
+        }
+        return null;
+    }
     // TODO: 2022-10-11 point type을 이용한 좌표조회로 변경 필요
 }
