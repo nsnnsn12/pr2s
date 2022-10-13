733704a404f6934ec0f5322df39567e76238b8e1
nsnnsn12 <gkdlshtjdrb@naver.com>
2022-10-13 20:06:00 +0900

    FEAT : 방 조회 테스트 클래스 생성

--- src/main/java/com/metacrew/pr2s/config/WebConfig.java
+++ src/main/java/com/metacrew/pr2s/config/WebConfig.java
@@ -3,7 +3,9 @@ package com.metacrew.pr2s.config;
 import com.metacrew.pr2s.config.interceptor.FileInterceptor;
 import com.metacrew.pr2s.repository.FileInfoRepository;
 import com.metacrew.pr2s.service.storageservice.StorageService;
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
@@ -11,9 +13,14 @@ import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
 @Configuration
 @RequiredArgsConstructor
 public class WebConfig implements WebMvcConfigurer {
+    @PersistenceContext
+    private EntityManager entityManager;
     private static final String[] CLASSPATH_RESOURCE_LOCATIONS = { "classpath:/static/" };
     private final StorageService storageService;
     private final FileInfoRepository fileInfoRepository;
@@ -34,5 +41,10 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/**").addResourceLocations(CLASSPATH_RESOURCE_LOCATIONS);
+    }
+
+    @Bean
+    public JPAQueryFactory jpaQueryFactory(){
+        return new JPAQueryFactory(entityManager);
     }
 }
